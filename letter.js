d55581a0810596edc7df8776df2e440c8785d095
UsenbekMuhammed <bakitdjulekbaev@gmail.com>
2026-06-22 11:30:40 +0500

    Update letter.js

--- letter.js
+++ letter.js
@@ -1,6 +1,6 @@
-const date = localStorage.getItem("dateDate");
-const time = localStorage.getItem("dateTime");
-const place = localStorage.getItem("datePlace");
+const date = localStorage.getItem("dateDate") || "не выбрана";
+const time = localStorage.getItem("dateTime") || "не выбрано";
+const place = localStorage.getItem("datePlace") || "не выбрано";
 
 document.getElementById("letterContent").innerHTML = `
   💖 Наше свидание запланировано 💖<br><br>
