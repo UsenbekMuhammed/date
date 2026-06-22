dc9d7fd2c76be26c7581ef891a1f3c0f7578c40d
UsenbekMuhammed <bakitdjulekbaev@gmail.com>
2026-06-22 12:00:24 +0500

    Update letter.js

--- letter.js
+++ letter.js
@@ -2,6 +2,9 @@ const date = localStorage.getItem("dateDate") || "не выбрана";
 const time = localStorage.getItem("dateTime") || "не выбрано";
 const place = localStorage.getItem("datePlace") || "не выбрано";
 
+const bgMusic = document.getElementById("bgMusic");
+const musicBtn = document.getElementById("musicBtn");
+
 document.getElementById("letterContent").innerHTML = `
   💖 Наше свидание запланировано 💖<br><br>
   Анель выбрала:<br><br>
@@ -10,3 +13,30 @@ document.getElementById("letterContent").innerHTML = `
   📍 Место: ${place}<br><br>
   Жду с нетерпением 😌💘
 `;
+
+const savedTime = localStorage.getItem("musicTime");
+const wasPlaying = localStorage.getItem("musicWasPlaying");
+
+if (savedTime) {
+  bgMusic.currentTime = Number(savedTime);
+}
+
+bgMusic.volume = 0.7;
+
+if (wasPlaying === "yes") {
+  bgMusic.play().then(() => {
+    musicBtn.innerText = "🔊";
+  }).catch(() => {
+    musicBtn.innerText = "🔇";
+  });
+}
+
+musicBtn.addEventListener("click", () => {
+  if (bgMusic.paused) {
+    bgMusic.play();
+    musicBtn.innerText = "🔊";
+  } else {
+    bgMusic.pause();
+    musicBtn.innerText = "🔇";
+  }
+});
